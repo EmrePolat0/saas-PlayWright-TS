57f2ed87b71d78b15b193557d87a5f827931f40b
EmrePolat0 <emre.polat0@outlook.com>
2025-07-19 22:11:17 -0700

    new commit

--- utils/DBUtils.ts
+++ utils/DBUtils.ts
@@ -17,4 +17,34 @@ export default class DBUtil {
     } catch (err) {
       throw new Error(`Error while executing query\n${err.message}`);
     }
+  }
+    /**
+   * Executes the query on Oracle database
+   * @param dbConfig data base configuration
+   * @param query to be executed
+   * @returns record set
+   */
+    public static async executeOracleQuery(dbConfig: string, query: string) {
+    const configs = dbConfig.split(CommonConstants.SEMICOLON);
+    const config = {
+      user: configs[0].replace(DBConstants.USER, CommonConstants.BLANK).trim(),
+      password: configs[1].replace(DBConstants.PASSWORD, CommonConstants.BLANK).trim(),
+      connectString: configs[2].replace(DBConstants.CONNECTION_STRING, CommonConstants.BLANK).trim(),
+    };
+    let connection: oracledb.Connection;
+    try {
+      connection = await oracledb.getConnection(config);
+      const result = await connection.execute(query);
+      return { rows: result.rows, rowsAffected: result.rowsAffected };
+    } catch (err) {
+      throw new Error(`Error while executing query\n${err.message}`);
+    } finally {
+      if (connection) {
+        try {
+          await connection.close();
+        } catch (err) {
+          console.error(err);
+        }
+      }
+    }
   }}
